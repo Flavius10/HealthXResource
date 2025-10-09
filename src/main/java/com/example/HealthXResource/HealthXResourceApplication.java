ca65e9ec765cabb18e6ec9661e3453e84f5c8025
Flavius10 <flaviusa1010@gmail.com>
2025-10-09 12:13:35 +0300

    Create the project

---
+++ src/main/java/com/example/HealthXResource/HealthXResourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.HealthXResource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthXResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthXResourceApplication.class, args);
+	}
+
+}
