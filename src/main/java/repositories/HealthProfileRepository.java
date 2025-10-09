21ac2837422d8b8b9bd93fa570e5f052fee3f814
Flavius10 <flaviusa1010@gmail.com>
2025-10-09 16:35:06 +0300

    Implement the HealthMetricService

--- src/main/java/repositories/HealthProfileRepository.java
+++ src/main/java/repositories/HealthProfileRepository.java
@@ -2,6 +2,10 @@ package repositories;
 
 import entities.HealthProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 public interface HealthProfileRepository extends JpaRepository<HealthProfile, Integer> {
+    Optional<HealthProfile> findHealthProfileByUsername(String username);
 }
--- src/main/java/services/HealthProfileService.java
+++ src/main/java/services/HealthProfileService.java
@@ -1,4 +1,56 @@
 package services;
 
+import entities.HealthMetric;
+import entities.HealthProfile;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import repositories.HealthMetricRepository;
+import repositories.HealthProfileRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class HealthProfileService {
+
+    private final HealthProfileRepository healthProfileRepository;
+    private final HealthMetricRepository healthMetricRepository;
+
+    public HealthProfileService(HealthProfileRepository healthProfileRepository,
+                                HealthMetricRepository healthMetricRepository) {
+        this.healthProfileRepository = healthProfileRepository;
+        this.healthMetricRepository = healthMetricRepository;
+    }
+
+    public void addHealthMetric(HealthMetric healthMetric){
+        Optional<HealthProfile> healthProfile =
+                healthProfileRepository.findHealthProfileByUsername(healthMetric.getProfile().getUsername());
+
+        healthProfile.ifPresentOrElse(
+                profile ->
+                {
+                    healthMetric.setProfile(profile);
+                    this.healthMetricRepository.save(healthMetric);
+                },
+                () -> {
+                    throw new RuntimeException("Profile not found");
+                }
+        );
+    }
+
+    public List<HealthMetric> findHealthMetricHistory(String username){
+        return this.healthMetricRepository.findHealthMetricHistory(username);
+    }
+
+    public void deleteHealMetricForUsers(String username){
+        Optional<HealthProfile> profile =
+                healthProfileRepository.findHealthProfileByUsername(username);
+
+        profile.ifPresentOrElse(healthMetricRepository::deleteAllForUser,
+                () -> {
+                    throw new RuntimeException("Profile not found");
+                }
+        );
+    }
 }
