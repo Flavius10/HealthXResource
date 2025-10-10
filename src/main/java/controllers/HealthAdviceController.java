da41557d4edd0add82025ef8976369e8474c3437
Flavius10 <flaviusa1010@gmail.com>
2025-10-10 12:19:28 +0300

    Implement the HealthAdviceController

--- src/main/java/controllers/HealthAdviceController.java
+++ src/main/java/controllers/HealthAdviceController.java
@@ -1,4 +1,27 @@
 package controllers;
 
+import controllers.dto.HealthAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/advice")
 public class HealthAdviceController {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(HealthAdviceController.class);
+
+    @PostMapping
+    public void provideHealthAdvice(
+            @RequestBody List<HealthAdvice> healthAdvices){
+        healthAdvices.forEach(healthAdvice ->
+                logger.info(healthAdvice.toString()));
+    }
+
 }
