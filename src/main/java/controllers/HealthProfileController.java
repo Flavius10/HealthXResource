cffd9431a84bda03edc19f2a2ee97625252003dd
Flavius10 <flaviusa1010@gmail.com>
2025-10-10 12:43:34 +0300

    Implement the HealthMetricController

--- src/main/java/controllers/HealthProfileController.java
+++ src/main/java/controllers/HealthProfileController.java
@@ -1,4 +1,33 @@
 package controllers;
 
+
+import entities.HealthProfile;
+import org.springframework.web.bind.annotation.*;
+import services.HealthProfileService;
+
+@RestController
+@RequestMapping("/profile")
 public class HealthProfileController {
+
+    private final HealthProfileService healthProfileService;
+
+    public HealthProfileController(HealthProfileService healthProfileService) {
+        this.healthProfileService = healthProfileService;
+    }
+
+    @PostMapping
+    public void addHealthProfile(@PathVariable HealthProfile profile){
+        this.healthProfileService.addHealthProfile(profile);
+    }
+
+    @GetMapping("/{username}")
+    public HealthProfile findHealthProfile(@PathVariable String username) {
+        return healthProfileService.findHealthProfile(username);
+    }
+
+    @DeleteMapping("/{username}")
+    public void deleteHealthProfile(@PathVariable String username){
+        this.healthProfileService.deleteHealthProfile(username);
+    }
+
 }
