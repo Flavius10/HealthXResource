8373490f04e32e396ec50af38dc10f31f3b4ceab
Flavius10 <flaviusa1010@gmail.com>
2025-10-10 12:44:34 +0300

    Implement the HealtgAdvice

--- src/main/java/controllers/dto/HealthAdvice.java
+++ src/main/java/controllers/dto/HealthAdvice.java
@@ -1,4 +1,24 @@
 package controllers.dto;
 
 public class HealthAdvice {
+
+    private String advice;
+    private String username;
+
+    public String getAdvice() {
+        return advice;
+    }
+
+    public void setAdvice(String advice) {
+        this.advice = advice;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
