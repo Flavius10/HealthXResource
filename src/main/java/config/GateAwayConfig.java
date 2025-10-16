d40acd2fe2e5b720054eabdc762f4e0acae42c1c
Flavius10 <flaviusa1010@gmail.com>
2025-10-16 11:17:50 +0300

    Implement the RouteLocator for routing /metric, /profile, /advice

--- src/main/java/config/GateAwayConfig.java
+++ src/main/java/config/GateAwayConfig.java
@@ -1,12 +1,30 @@
 package config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GateAwayConfig {
 
     @Value("${bussines.logic.server.url}")
-    private String Url;
+    private String bussinesLogicServerUrl;
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder ){
+        return builder.routes().
+                route(p -> p
+                        .path("/metric/**")
+                        .uri(bussinesLogicServerUrl + "/metric"))
+                .route(p -> p
+                        .path("/profile")
+                        .uri(bussinesLogicServerUrl + "/profile"))
+                .route(p -> p
+                        .path("/advice/**")
+                        .uri(bussinesLogicServerUrl + "/advice"))
+                .build();
+    }
 
 }
